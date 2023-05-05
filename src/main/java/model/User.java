4c61d42403bb78a9d64df8d91456f08a7ea56a54
Iaroslav <eremeev.pt@gmail.com>
2023-05-05 23:58:18 +0300

    Update User class

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,16 +1,29 @@
 package model;
 
+import com.fasterxml.jackson.annotation.JsonTypeId;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
+import javax.persistence.*;
+
+@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @Column(unique = true)
+    @NonNull
     private String login;
+    @NonNull
     private String password;
+    @NonNull
     private String name;
+    @NonNull
     private String surname;
 }
