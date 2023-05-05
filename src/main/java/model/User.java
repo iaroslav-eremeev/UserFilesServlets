4d7b795776c4fd2e381385caab70d1de99d97193
Iaroslav <eremeev.pt@gmail.com>
2023-05-05 23:43:20 +0300

    Add User class

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,4 +1,16 @@
 package model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    private long id;
+    private String login;
+    private String password;
+    private String name;
+    private String surname;
 }
